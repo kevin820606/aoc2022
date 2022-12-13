a708322994c731cf74b66b016998799badea3d29
Kevin_laptop <kevin820606@gmail.com>
2022-12-13 19:16:48 +0800

    I dont know why

--- src/day12.py
+++ src/day12.py
@@ -1,7 +1,6 @@
 from src.utils import print_ans, read_aoc, AOCFILE
 from typing import TypedDict, NamedTuple
 
-
 class Coordinate(NamedTuple):
     x: int
     y: int
@@ -35,14 +34,17 @@ def get_coords(
                     coord=coord,
                     height=height,
                     children=[],
-                    is_check=False
+                    is_check=False,
+                    parents=set(),
+                    distance_from_start=1000000
                 )
             }
     return (all_coord, start, end)
 
 
 class Map:
-    def __init__(self, nodes: dict[Coordinate, Node]):
+    def __init__(self, nodes: dict[Coordinate, Node], end):
+        self.end = end
         self.nodes = nodes
 
     def get_four_directions(self, coord: Coordinate) -> list[Coordinate]:
@@ -61,38 +63,23 @@ class Map:
                 if self.nodes.get(dir):
                     if self.nodes[dir]["height"] - self.nodes[node]["height"] in [0, 1]:
                         self.nodes[node]["children"].append(dir)
-                        # self.nodes[dir]["parents"].add(node)
 
-    def get_minimal_distance_from_start(self, start: Coordinate) -> None:
-        self.nodes[start]["distance_from_start"] = 0
-        check_list = [start]
-        while check_list:
-            coord = check_list.pop(0)
-            children = self.nodes[coord]["children"]
-            for child in children:
-                if (not coord in self.nodes[child]["parents"]):
-                    continue
-                self.nodes[child]["parents"].remove(coord)
-                if (
-                    self.nodes[child]["distance_from_start"]
-                    >= self.nodes[coord]["distance_from_start"] + 1
-                ):
-                    self.nodes[child]["distance_from_start"] = (
-                        self.nodes[coord]["distance_from_start"] + 1
-                    )
-                check_list.append(child)
-
-    def get_start_to_end(self, start:Coordinate, end:Coordinate) -> None:
+    def get_start_to_end(self, start:Coordinate) -> None:
         node = start
         children = self.nodes[node]["children"]
         layer = 1
         next_children:list[Coordinate] = []
+
         while children:
-            child = children.pop(0)
+            node = children.pop(0)
 
-            next_children.extend(self.nodes[child]["children"])
+            if not self.nodes[node]["is_check"] or self.nodes[node]["distance_from_start"] > layer:
+                self.nodes[node]["distance_from_start"] = layer
+                self.nodes[node]["is_check"] = True
+                next_children.extend(self.nodes[node]["children"])
 
-            if end in next_children:
+            if self.end in next_children:
+                print(node)
                 print(layer + 1)
                 break
 
@@ -100,17 +87,23 @@ class Map:
                 layer += 1
                 children = list(set(next_children))
                 next_children = []
-                if layer > 900:
-                    print(children)
-                print(layer)
+
+    def try_not_reach(self):
+        for k, v in self.nodes.items():
+            if not v["is_check"]:
+                print(k)
+                self.get_start_to_end(start = k)
 
 def Q1():
     aoc_file = read_aoc(12)
     all_cord, start, end = get_coords(aoc_file=aoc_file)
-    questionmap = Map(nodes=all_cord)
+    questionmap = Map(nodes=all_cord, end = end)
+    questionmap.nodes[start]["distance_from_start"] = 0
     questionmap.get_node_chain()
-    questionmap.get_start_to_end(start=start, end=end)
-
+    questionmap.get_start_to_end(start=start)
+    questionmap.try_not_reach()
+    # from pprint import pprint
+    # pprint(questionmap.nodes)
 
 if __name__ == "__main__":
     Q1()
