ab51618df54dc1649b20b02fda04be1254c811f1
Kevin_laptop <kevin820606@gmail.com>
2022-12-13 20:26:19 +0800

    YOU CAN GO FUCKING DOWN

--- src/day12.py
+++ src/day12.py
@@ -51,9 +51,9 @@ class Map:
         x, y = coord
         return [
             Coordinate(x=x + 1, y=y),
-            Coordinate(x=x - 1, y=y),
             Coordinate(x=x, y=y + 1),
-            Coordinate(x=x, y=y - 1)
+            Coordinate(x=x - 1, y=y),
+            Coordinate(x=x, y=y - 1),
         ]
 
     def get_node_chain(self) -> None:
@@ -61,38 +61,32 @@ class Map:
             four_directions = self.get_four_directions(node)
             for dir in four_directions:
                 if self.nodes.get(dir):
-                    if self.nodes[dir]["height"] - self.nodes[node]["height"] in [0, 1]:
+                    if self.nodes[dir]["height"] - self.nodes[node]["height"] < 2:
                         self.nodes[node]["children"].append(dir)
 
     def get_start_to_end(self, start:Coordinate) -> None:
         node = start
         children = self.nodes[node]["children"]
         layer = 1
-        next_children:list[Coordinate] = []
-
+        next_layer_nodes:list[Coordinate] = []
+        self.checked_list = []
         while children:
             node = children.pop(0)
 
-            if not self.nodes[node]["is_check"] or self.nodes[node]["distance_from_start"] > layer:
-                self.nodes[node]["distance_from_start"] = layer
-                self.nodes[node]["is_check"] = True
-                next_children.extend(self.nodes[node]["children"])
+            self.checked_list.append(node)
+            for child in self.nodes[node]["children"]:
+                if child not in self.checked_list:
+                    next_layer_nodes.append(child)
 
-            if self.end in next_children:
+            if self.end in next_layer_nodes:
                 print(node)
                 print(layer + 1)
                 break
 
             if not children:
                 layer += 1
-                children = list(set(next_children))
-                next_children = []
-
-    def try_not_reach(self):
-        for k, v in self.nodes.items():
-            if not v["is_check"]:
-                print(k)
-                self.get_start_to_end(start = k)
+                children = list(set(next_layer_nodes))
+                next_layer_nodes = []
 
 def Q1():
     aoc_file = read_aoc(12)
@@ -101,9 +95,6 @@ def Q1():
     questionmap.nodes[start]["distance_from_start"] = 0
     questionmap.get_node_chain()
     questionmap.get_start_to_end(start=start)
-    questionmap.try_not_reach()
-    # from pprint import pprint
-    # pprint(questionmap.nodes)
 
 if __name__ == "__main__":
     Q1()
